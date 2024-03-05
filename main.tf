ff9561ba902ad53d49884738e00a7e238b506e83
pahardy <hardy_patrick@yahoo.ca>
2024-03-05 13:54:00 -0500

    amend issues with EBS CSI Add-on

--- main.tf
+++ main.tf
@@ -73,73 +73,28 @@ Large contribution here from Stacksimplify (via Udemy course):
 https://github.com/stacksimplify/terraform-on-aws-eks/blob/main/18-EBS-CSI-Install-using-EKS-AddOn/02-ebs-addon-terraform-manifests/c4-01-ebs-csi-datasources.tf
 */
 
-#get the EBS CSI IAM policy
-data "http" "ebs_csi_iam_policy" {
-  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json"
-
-  # Optional request headers
-  request_headers = {
-    Accept = "application/json"
-  }
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
-#Acquiring the policy from AWS
-resource "aws_iam_policy" "ebs_csi_iam_policy" {
-  name = "${var.aws_region}-AmazonEKS_EBS_CSI_Driver_Policy"
-  description = "EBS CSI IAM Policy for the EKS cluster"
-  path = "/"
-  policy = data.http.ebs_csi_iam_policy.response_body
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
-output "ebs_csi_iam_policy_arn" {
-  value = aws_iam_policy.ebs_csi_iam_policy.arn
-}
-
-#Create an IAM role and associate it with the policy created above
-resource "aws_iam_role" "ebs_csi_iam_role" {
-  name = "${var.aws_region}-ebs-csi-iam-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Federated = "${module.eks.oidc_provider_arn}"
-        }
-        Condition = {
-          StringEquals = {
-            "${module.eks.oidc_provider_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-          }
-        }
-
-      },
-    ]
-  })
-
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
-    Environment = "terraform"
-    Owner = "Patrick"
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
   }
-}
-
-# Associate EBS CSI IAM Policy to EBS CSI IAM Role
-resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
-  policy_arn = aws_iam_policy.ebs_csi_iam_policy.arn
-  role       = aws_iam_role.ebs_csi_iam_role.name
-}
-
-output "ebs_csi_iam_role_arn" {
-  description = "EBS CSI IAM Role ARN"
-  value = aws_iam_role.ebs_csi_iam_role.arn
-}
-
-#Installation of EBS CSI addon
-resource "aws_eks_addon" "ebs_eks_addon" {
-  depends_on = [aws_iam_role_policy_attachment.ebs_csi_iam_role_policy_attach]
-  cluster_name = module.eks.cluster_name
-  addon_name   = "aws-ebs-csi-driver"
-  service_account_role_arn = aws_iam_role.ebs_csi_iam_role.arn 
 }
